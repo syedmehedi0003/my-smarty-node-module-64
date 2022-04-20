c6e3c833e10dfec422307ff9288014ea343c8485
syedmehedi0003 <mehedihasanbahar.com>
2022-04-21 05:28:50 +0600

    new commit

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ const users = [
     { id: 7, name: 'Mabana', email: 'naban@gmail.com', phone: '0183627565' },
 ]
 
+// app.get('/users', (req, res) => {
+//     res.send(users);
+// });
+
 app.get('/users', (req, res) => {
     // console.log('Query', req, query);
     //filter by search query parameter
